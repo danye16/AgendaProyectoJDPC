485d679bf00892253e1df562e918bb46b5a5fa3f
PC <danyel.16@gmail.com>
2024-01-28 17:40:01 -0600

    Agregar archivos de proyecto.

---
+++ AgendaProyectoJDPC/Conexion/Cconexion.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Firebase.Database;
+
+namespace AgendaProyectoJDPC.Conexion
+{
+    public class Cconexion
+    {
+        public static FirebaseClient firebase= new FirebaseClient("https://mvmagenda-9341b-default-rtdb.firebaseio.com/");
+    }
+}
---
+++ AgendaProyectoJDPC/Modelo/Magenda.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AgendaProyectoJDPC.Modelo
+{
+    public class Magenda
+    {
+        public Guid Id { get; set; }
+        public string Titulo { get; set; }
+        public string Texto { get; set; }
+    }
+}
